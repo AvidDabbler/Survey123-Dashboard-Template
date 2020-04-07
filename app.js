96fc71d8e510977309753d6e82bdcb60d964fb69
AvidDabbler <walter.k.jenkins@gmail.com>
2020-04-07 17:54:13 -0500

    working search function

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import { surveyID as surveyID, jsonURL as jsonURL} from './private.js';
 
+
+
+// JAVASCRIPT VARIABLES
+let facilities, filtered_facilities;
+let filtered = false;
+
+
+
 //STATIC URLS
 const survey123Url = jsonURL();
 
@@ -21,14 +29,48 @@ if (!Element.prototype.matches) {
       return null;
 }};
 
-const render = async () => {
+
+const clear_data = async () => {
+    list_div.innerHTML = '';
+    return;
+};
+
+
+// DATA PROCESSING
+const filter_data = (data, input) => {
+    const d = data.filter(d => {
+        return new RegExp('^' + input.replace(/\*/g, '.*') + '$').test(d.properties.requesting_facility)
+        // return d.features.properties.vehicle_number == `${input}*`
+    });
+    filtered_facilities = d;
+    filtered = true;
+    return d
+};
+
+
+const get_survey_data = async () => {
+    console.log('fetch');
+    const response = await fetch(survey123Url);
+    console.log(response);
+
+    const json = await response.json();
+    console.log(json);
+
+    let data = json.features;
+    console.log('get_survey_data', data);
+    facilities = data;
+    return data;
+}
+
+
+const render = async (d) => {
     // DATA
     list_div.innerHTML = "<p class='i'>Data is loading...</p>";
     const response = await fetch(survey123Url);
     const json = await response.json();
     const data = json.features;
 
-    const sorted_data = data.sort(function (a, b) {
+    const sorted_data = d.sort(function (a, b) {
         if (a.properties.new_requesting_facility <b.properties.new_requesting_facility) {
             return -1;
         }
@@ -59,17 +101,44 @@ const render = async () => {
 
 };
 
+const searching = async (value) => {
+    filtered = true;
+
+    list_div.innerHTML = 'Data is loading...';
+    clear_data();
+    let fv = filter_data(facilities, `${value}*`);
+    console.log(fv);
+    render(await fv);
+    return;
+};
+
 
 const clickEvent = (event) => {
     event.preventDefault();
-    const iframe = document.getElementById('ifrm');
+    const iframe_exists = document.getElementById('iframe');
+    const iframe = event.target.closest('#iframe');
+    const search = event.target.closest('#search');
+    const search_bar = document.getElementById('search-bar').value;
 
-
-    if(!event.target.closest('#iframe') && iframe){
+    if(!iframe && iframe_exists){
         console.log('iframe present')
         iframe.parentNode.removeChild(iframe);
-        return
-    }else if(!event.target.closest('.openpop') && !iframe){
+        return;
+
+    
+    // SEARCH CLICK!!!
+    }else if(search){
+        console.log('search')
+        if(search_bar != ''){
+            console.log('search 1', search_bar)
+            searching(search_bar);
+        }else if(search_bar == '' && filtered){
+            console.log('search 2')
+            render(vehicles)
+            return;
+        }
+    // CLICK LIST ELEMENT AND OPEN IFRAME!!!
+    }else if(!event.target.closest('.openpop')){
         console.log('Not list_div');
         return;
     }else{        
@@ -83,18 +152,15 @@ const clickEvent = (event) => {
         ifrm.setAttribute('id', 'ifrm'); // assign an id
         ifrm.setAttribute(`src`, url);
 
-        //document.body.appendChild(ifrm); // to place at end of document
-
-        // // to place before another page element
+        // to place before another page element
         var el = document.getElementById('marker');
         main.parentNode.insertBefore(ifrm, el);
-
-        // // assign url
-        // ifrm.setAttribute('src', 'demo.html');
 
     }
 }
 
 
-render();
+get_survey_data().then(data =>{
+    render(facilities);
+});
 window.addEventListener("click", clickEvent, false)
